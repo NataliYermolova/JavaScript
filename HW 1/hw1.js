d4cfd331dc36da5e6d05df352523d9433aab0c3d
Yermolova Nataliia <yermolova.digital@gmail.com>
2023-11-20 22:48:37 +0200

    done

---
+++ HW 1/hw1.js
@@ -0,0 +1,37 @@
+// 1
+let s1 = 'hello'
+let s2 = 'owu'
+let s3 = 'com'
+let s4 = 'ua'
+
+let n1 = 1
+let n2 = 10
+let n3 = -999
+let n4 = 123
+let n5 = 3.14
+let n6 = 2.7
+let n7 = 16
+
+let b1 = true
+let b2 = false
+
+console.log(s1, s2, s3, s4, n1, n2, n3, n4, n5, n6, n7, b1, b2)
+
+
+// 2
+let firstName = 'Nataliia'
+let middleName = 'Oleksandrivna'
+let lastName = 'Yermolova'
+let person = (`${firstName} ${middleName} ${lastName}`)
+
+console.log(person)
+
+
+// 3
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
